150d9035b1a1d122955b85659e002af8e93b03fd
ivaaak <43663336+ivaaak@users.noreply.github.com>
2022-03-20 11:19:47 +0200

    initial upload

---
+++ ASP.NET SuperHeroAPI/Data/Models/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperHeroAPI.Data.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+	  
+        public string? Name { get; set; }
+	  
+        public string? FirstName { get; set; }
+	  
+        public string? LastName { get; set; }
+	  
+        public string? Place { get; set; }
+    }
+}
