d9f97a45b830ce72291cc47b2f0571dbd005f53c
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-05-25 10:58:31 +0300

    added models and repos

---
+++ ASP.NET MinimalAPI Auth/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinimalJwt.Models;
+
+namespace MinimalJwt.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new()
+        {
+            new() { Username = "admin", EmailAddress = "admin@email.com", Password = "password", GivenName = "Dani", Surname = "Danielov", Role = "Administrator" },
+            new() { Username = "standard", EmailAddress = "standard@email.com", Password = "password", GivenName = "Gosho", Surname = "Goshov", Role = "Standard" },
+        };
+    }
+}
